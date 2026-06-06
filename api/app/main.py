c298b855392c7903ead2046421a5039b23224392
Edson Eddy <crew0eddy@gmail.com>
2026-06-06 19:09:07 -0400

    chore: add production docker configuration with multi-stage build and nginx

--- api/app/main.py
+++ api/app/main.py
@@ -26,9 +26,15 @@ class AnalysisRequest(BaseModel):
 
 app = FastAPI(title="CSIM Online API")
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://localhost",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
