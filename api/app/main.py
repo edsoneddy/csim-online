624fa40e81f0a96f4f426bf32717c4418bbe8f36
Edson Eddy <crew0eddy@gmail.com>
2026-07-17 23:06:59 -0400

    feat: add .dockerignore and initialize __init__.py; remove main.py startup command

--- api/app/main.py
+++ api/app/main.py
@@ -42,10 +42,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
-
 @app.get("/")
 def read_root():
     return {"status": "API is running smoothly"}
